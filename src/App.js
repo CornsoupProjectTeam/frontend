201a3126bbf5fc2db445c4a385c00ca0fcfc25ca
ujin6666 <ujin736@gmail.com>
2024-08-09 17:52:54 +0900

    Commit message for frontend project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/data')  // 절대 경로 사용
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +33,14 @@ function App() {
         >
           Learn React
         </a>
+        {data ? (
+          <div>
+            <h3>Data from Flask:</h3>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          </div>
+        ) : (
+          <p>Loading data from Flask...</p>
+        )}
       </header>
     </div>
   );
